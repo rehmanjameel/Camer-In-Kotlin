ab1a1ead97e7dca08bb490dff89de686c9e32b77
rehmanjameel <abdulrehmancs17@gmail.com>
2022-02-16 14:23:03 +0500

    Simple camera

---
+++ app/src/main/java/com/example/opencamera/MainActivity.kt
@@ -0,0 +1,141 @@
+package com.example.opencamera
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.ContentValues
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import android.widget.Button
+import android.widget.ImageView
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
+import androidx.core.content.PermissionChecker
+import com.bumptech.glide.Glide
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+
+@RequiresApi(Build.VERSION_CODES.M)
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var resultLauncher : ActivityResultLauncher<Intent>
+    private lateinit var capturedImage: ImageView
+    private lateinit var cameraButton: Button
+    private var currentImagePath: String? = null
+    private var imageUri: Uri? = null
+    val CAMERA_PERMISSION_CODE = 1000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        capturedImage = findViewById(R.id.circleImageViewId)
+        cameraButton = findViewById(R.id.captureImageId)
+        //result of open camera
+        resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                Glide.with(applicationContext)
+                    .load(imageUri)
+                    .error(R.drawable.ic_baseline_person_24)
+                    .into(capturedImage)
+                Log.e("Failed", "$imageUri")
+
+//                handleCameraImage(result.data)
+            } else {
+                Log.e("Failed", "Failed to take picture")
+            }
+        }
+
+        cameraButton.setOnClickListener {
+            val permissionGranted = requestCameraPermission()
+            if (permissionGranted) {
+                openCameraInterface()
+            }
+        }
+    }
+
+    @SuppressLint("WrongConstant")
+    private fun requestCameraPermission(): Boolean {
+        var permissionGranted = false
+        // If system os is Marshmallow or Above, we need to request runtime permission
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            val cameraPermissionNotGranted = PermissionChecker.checkSelfPermission(applicationContext,
+                Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED
+            if (cameraPermissionNotGranted){
+                val permission = arrayOf(Manifest.permission.CAMERA)
+
+                // Display permission dialog
+                requestPermissions(permission, CAMERA_PERMISSION_CODE)
+            }
+            else{
+                // Permission already granted
+                permissionGranted = true
+            }
+        }
+        else{
+            // Android version earlier than M -&gt; no need to request permission
+            permissionGranted = true
+        }
+
+        return permissionGranted
+    }
+
+    private fun openCameraInterface() {
+        val values = ContentValues()
+
+        values.put(MediaStore.Images.Media.TITLE, "Take Picture")
+        values.put(MediaStore.Images.Media.DESCRIPTION, "Image Description")
+        imageUri = contentResolver?.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+
+        //Launch camera
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri)
+        resultLauncher.launch(cameraIntent)
+    }
+
+    private fun handleCameraImage(intent: Intent?) {
+//        Glide.with(applicationContext)
+//            .load(intent?.extras?.get("data"))
+//            .error(R.drawable.ic_baseline_person_24)
+//            .into(capturedImage)
+
+        val bitmap = intent?.extras?.get("data") as Bitmap
+        capturedImage.setImageBitmap(bitmap)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == CAMERA_PERMISSION_CODE) {
+            if (grantResults.size == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                // Permission was granted
+                openCameraInterface()
+            }
+            else{
+                // Permission was denied
+                showAlert("Camera permission was denied. Unable to take a picture.");
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    private fun showAlert(message: String) {
+        val builder = MaterialAlertDialogBuilder(this)
+        builder.setPositiveButton("Ok") {_, _ ->
+            builder.create().dismiss()
+        }
+        builder.setTitle("Permission!")
+        builder.setMessage(message)
+        builder.create().show()
+    }
+}
